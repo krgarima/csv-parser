f73d100e573a915ac18c4f4a8c3b95fb5175e271
krgarima <kumargarima59@gmail.com>
2026-04-26 16:59:06 +0530

    chore(client): vite, tailwind, tsconfig, eslint scaffold with shadcn config

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  env: { browser: true, es2022: true },
+  settings: { react: { version: '18.3' } },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.cjs', '*.config.*'],
+};
